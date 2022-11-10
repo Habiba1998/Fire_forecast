1bf3781cb8005ba9827b46e8d29e90edda868201
Habiba Mounir <hana612.hm@gmail.com>
2022-11-10 10:25:21 -0800

    Create TCPClient Derived Class

---
+++ Fire_forecast/include/TCPClient.h
@@ -0,0 +1,21 @@
+#ifndef TCPCLIENT
+#define TCPCLIENT
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "Client.h"
+
+class TCPClient: public Client
+{
+private: 
+	int client_socket;								// The socket of client if it is connected
+		 
+		 
+public:
+	TCPClient();
+	bool connect_server(struct sockaddr_in address, bool accumulate = false);		// accumulate option to allow accumulating results
+																					// To previous connection results
+	void read_server();
+	virtual ~TCPClient();
+};
+#endif
+
