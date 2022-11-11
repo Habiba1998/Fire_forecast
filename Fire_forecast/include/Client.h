2af2aa17ae20ff5f3d156a340a8e9ee65ce53d24
Habiba Mounir <hana612.hm@gmail.com>
2022-11-11 09:55:59 -0800

    Add Setters, Getters and Modify Accessibility

--- Fire_forecast/include/Client.h
+++ Fire_forecast/include/Client.h
@@ -4,37 +4,35 @@
 #include <mutex>
 class Client
 {
-protected:
-	
-	static std::mutex print_mtx;			// Mutex to ensure atomic execution of std::cout on printing if clients run on different threads 
-	bool connected;							// Indicates if the client is connected to a server
+private:
 	double accumulation;					// The calculated accumulation
 	double average;							// The calculated average
 	unsigned int readings_number;			// Equals to the number of seconds as server sends data each second
+	bool connected;							// Indicates if the client is connected to a server
+	std::mutex con_mtx;						// Mutex to ensure atomic update of connected
 	std::mutex acc_mtx;						// Mutex to ensure atomic update of the accumulation
 	std::mutex av_mtx;						// Mutex to ensure atomic update of the average
+
+protected:
+	
+	static std::mutex print_mtx;			// Mutex to ensure atomic execution of std::cout on printing if clients run on different threads 
+
 	int print_duration;						// Duration in millisecond to print the data
+
+	void set_accumulation(double a);
+	void set_average(double a);
+	void set_readings_number(unsigned int a);
+	void set_connected(bool v);
+	void update(double reading);
 
 public:
 	Client(int d = 5000);					// Default duration = 5 seconds
-	unsigned int get_readings_number()
-	{
-		return readings_number;
-	}
-	bool get_connected()
-	{
-		return connected;
-	}
+	unsigned int get_readings_number();		
+	bool get_connected();
+	double get_accumulation();
+	double get_average();
 	
-	double get_accumulation()
-	{
-		return accumulation;
-	}
-	double get_average()
-	{
-		return average;
-	}
-	void update(double reading);
+	
 	void handleprint();
 	void print(double av, double acc);
 	virtual ~Client() {}
--- Fire_forecast/src/Client.cpp
+++ Fire_forecast/src/Client.cpp
@@ -8,6 +8,8 @@ using namespace std;
 std::mutex Client::print_mtx;
 
 
+
+
 Client::Client(int d)
 {
 	print_duration = d;
@@ -15,6 +17,70 @@ Client::Client(int d)
 	accumulation = 0;
 	average = 0;
 	
+}
+// Setters
+void Client::set_accumulation(double a)
+{
+	acc_mtx.lock();
+	accumulation = a;
+	acc_mtx.unlock();
+}
+
+void Client::set_average(double a)
+{
+	av_mtx.lock();
+	average = a;
+	av_mtx.unlock();
+}
+
+void Client::set_readings_number(unsigned int a)
+{
+	acc_mtx.lock();
+	readings_number = a;
+	acc_mtx.unlock();
+}
+void Client::set_connected(bool v)
+{
+	con_mtx.lock();
+	connected = v;
+	con_mtx.unlock();
+}
+
+// Getters
+bool Client::get_connected()
+{
+	bool r;
+	con_mtx.lock();
+	r = connected;
+	con_mtx.unlock();
+	return r;
+}
+unsigned int Client::get_readings_number()
+{
+	unsigned int v;
+	av_mtx.lock();
+	v = readings_number;
+	av_mtx.unlock();
+
+	return v;
+}
+
+double Client::get_accumulation()
+{
+	double v;
+	acc_mtx.lock();
+	v = accumulation;
+	acc_mtx.unlock();
+	return v;
+}
+
+double Client::get_average()	
+{
+	double v;
+	av_mtx.lock();
+	v = average;
+	av_mtx.unlock();
+	return v;
 }
 
 // Function to update data based on the reading
@@ -39,18 +105,16 @@ void Client::handleprint()
 
 
 	// The loop executes while the client is connected to a server
-	while (connected)
+	while (get_connected())
 	{
 		ms = std::chrono::steady_clock::now() + std::chrono::milliseconds(print_duration);
 		if (!first_iteration)
 		{
 			// Access variables using mutex locks
-			av_mtx.lock();
-			current_average = average;
-			av_mtx.unlock();
-			acc_mtx.lock();
-			current_accumulation = accumulation;
-			acc_mtx.unlock();
+			
+			current_average = get_average();
+			current_accumulation = get_accumulation();
+			
 
 			// Print the data
 			print_mtx.lock();
